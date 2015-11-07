6d391d521d864b15c54e69a5c70ab9de6615a732
陶威 <will_tao@126.com>
2015-11-07 13:58:03 +0800

    -todtao's blog

---
+++ static/js/duoshuo.js
@@ -0,0 +1,15 @@
+/**
+ * Created by taowei on 15/11/7.
+ */
+<!-- 多说公共JS代码 start (一个网页只需插入一次) -->
+var duoshuoQuery = {short_name: "todtao"};
+(function () {
+    var ds = document.createElement('script');
+    ds.type = 'text/javascript';
+    ds.async = true;
+    ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
+    ds.charset = 'UTF-8';
+    (document.getElementsByTagName('head')[0]
+    || document.getElementsByTagName('body')[0]).appendChild(ds);
+})();
+<!-- 多说公共JS代码 end -->
